ddb9067165dc07e99c6f0c853194e9ecf0f3f1de
Joshua Smith <joshuasmith@Joshs-MacBook-Pro.local>
2020-01-16 15:38:34 -0700

    completed first section

--- practice.js
+++ practice.js
@@ -15,6 +15,11 @@
 */
 
 //Code here
+let me = {
+  name: "Josh",
+  age: 31
+}
+alert(me.name);
 
 ////////// PROBLEM 2 //////////
 
@@ -24,6 +29,14 @@
 */
 
 //Code here
+let favoriteThings = {
+  band: "Hands Like Houses",
+  food: "Chinese",
+  person: "Alexie",
+  book: "Catcher in the Rye",
+  movie: "The Prestige",
+  holiday: "Thanksgiving"
+};
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
@@ -32,12 +45,17 @@
 
 //Code here
 
+favoriteThings.car = "Shelby Cobra";
+favoriteThings.brand = "Volcom";
+
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 ////////// PROBLEM 3 //////////
 
@@ -50,12 +68,20 @@
 */
 
 //Code here
+let backPack = {};
+
+let item = "firstPocket";
+
+backPack[item] = "chapstick";
+
+backPack.color = "blue";
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
+alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -64,6 +90,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -85,6 +112,8 @@ var user2 = {
 */
 
 //Code Here
+user2.name = "Bryan G. Smith";
+user2.email = "bryan.smith@devmounta.in";
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -96,6 +125,7 @@ var user2 = {
 */
 
 //Code Here
+
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
